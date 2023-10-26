6b0c3a2d589fb9418ff629c82db2cda388cfc12a
ing-arriola <ingeniero.arriola@gmail.com>
2023-10-26 16:19:02 -0600

    added colors

---
+++ src/constants/colors.ts
@@ -0,0 +1,11 @@
+export default {
+  primary: '#334FFA',
+  primaryDisabled: '#7e9afc',
+  secondary: '#CFD6FF',
+  background: '#F8F8F8',
+  white: '#FFFFFF',
+  textPrimary: '#020202',
+  textSecondary: '#9B9898',
+  success: '#00B833',
+  danger: '#FF0000',
+};
