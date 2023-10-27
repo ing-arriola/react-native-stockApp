87fddd24f6c9a8955e813af2f3bc1cc41e1f0ae6
ing-arriola <ingeniero.arriola@gmail.com>
2023-10-26 22:37:46 -0600

    added interface for stock

---
+++ src/interfaces/Stock.ts
@@ -0,0 +1,12 @@
+export interface Stock {
+  currency: string;
+  description: string;
+  displaySymbol: string;
+  figi: string;
+  isin: null;
+  mic: string;
+  shareClassFIGI: string;
+  symbol: string;
+  symbol2: string;
+  type: string;
+}
