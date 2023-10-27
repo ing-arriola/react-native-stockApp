1afe340cbd6b32b94278fbaeb78d5fd2fd6522ac
ing-arriola <ingeniero.arriola@gmail.com>
2023-10-26 22:37:30 -0600

    inject FinnhubApiService into App.tsx

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 /* eslint-disable react/react-in-jsx-scope */
 import {AppNavigator} from './src/navigation/AppNavigator';
 import {StockProvider} from './src/context/Stockcontext';
+import FinnhubApiService from './src/services/FinnhubApiService';
+import axios from 'axios';
+
+const finnhub = 'https://finnhub.io/api/v1/';
+const finnhubApi = axios.create({baseURL: finnhub});
+const finnhubApiService = new FinnhubApiService(finnhubApi);
 
 export default function App() {
   return (
-    <StockProvider>
+    <StockProvider finnhubApiService={finnhubApiService}>
       <AppNavigator />
     </StockProvider>
   );
