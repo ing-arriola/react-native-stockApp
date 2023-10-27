bb8b637edf36af68c9ebdd38caf59c3d42bb87dd
ing-arriola <ingeniero.arriola@gmail.com>
2023-10-26 22:36:58 -0600

    Inject FinnhubApiService into StockProvider

--- src/context/Stockcontext.tsx
+++ src/context/Stockcontext.tsx
@@ -1,16 +1,20 @@
 import React, {createContext, useState, useContext} from 'react';
+import FinnhubApiService from '../services/FinnhubApiService';
+import {Stock} from '../interfaces/Stock';
 
 interface StockContextType {
   stocksToWatch: stockTobeWatched[] | undefined;
   setstocksToWatch: React.Dispatch<
     React.SetStateAction<stockTobeWatched[] | undefined>
   >;
+  getStockData: (endpoint: string) => Promise<Stock[]>;
 }
 
 const StockContext = createContext<StockContextType | undefined>(undefined);
 
 interface Props {
   children: JSX.Element;
+  finnhubApiService: FinnhubApiService;
 }
 
 type stockTobeWatched = {
@@ -18,13 +22,29 @@ type stockTobeWatched = {
   alert: string;
 };
 
-export const StockProvider = ({children}: Props) => {
+export const StockProvider: React.FC<Props> = ({
+  children,
+  finnhubApiService,
+}) => {
   const [stocksToWatch, setstocksToWatch] = useState<
     stockTobeWatched[] | undefined
   >();
 
+  const getStockData = async (endpoint: string) => {
+    try {
+      const response = await finnhubApiService.getStockData(endpoint);
+      // Process the response as needed
+      return response.data;
+    } catch (error) {
+      // Handle errors
+      console.error('Error fetching stock data', error);
+      return null;
+    }
+  };
+
   return (
-    <StockContext.Provider value={{stocksToWatch, setstocksToWatch}}>
+    <StockContext.Provider
+      value={{stocksToWatch, setstocksToWatch, getStockData}}>
       {children}
     </StockContext.Provider>
   );
