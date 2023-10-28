c92e0e9adcdeeeed4540df011a5266567fc4920d
ing-arriola <ingeniero.arriola@gmail.com>
2023-10-28 07:52:59 -0600

    added border

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -3,6 +3,7 @@ export default {
   primaryDisabled: '#7e9afc',
   secondary: '#CFD6FF',
   background: '#F8F8F8',
+  border: '#e3e4e6',
   white: '#FFFFFF',
   textPrimary: '#020202',
   textSecondary: '#9B9898',
